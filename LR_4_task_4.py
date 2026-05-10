43dce5160f20f423a96a7b64e1f9e1a820154311
sofiakrivsha <sofiakrivsha2005@gmail.com>
2026-05-10 16:54:45 +0300

    LR_4_task_4.py

---
+++ LR_4_task_4.py
@@ -0,0 +1,42 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_squared_error, explained_variance_score
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+# Завантаження даних про нерухомість (оновлений спосіб)
+data_url = "http://lib.stat.cmu.edu/datasets/boston"
+raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
+data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
+target = raw_df.values[1::2, 2]
+feature_names = np.array(['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT'])
+# Перемішування та розбиття даних 
+X, y = shuffle(data, target, random_state=7)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=7)
+# Навчання регресора AdaBoost 
+# Використовуємо дерево рішень як основу
+regressor = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4), 
+                              n_estimators=400, random_state=7)
+regressor.fit(X_train, y_train)
+# Оцінка ефективності 
+y_pred = regressor.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+evs = explained_variance_score(y_test, y_pred)
+print("\nADABOOST REGRESSOR")
+print("-" * 20)
+print(f"Mean squared error = {round(mse, 2)}")
+print(f"Explained variance score = {round(evs, 2)}")
+# Обчислення важливості ознак 
+feature_importances = regressor.feature_importances_
+feature_importances = 100.0 * (feature_importances / max(feature_importances))
+index_sorted = np.flipud(np.argsort(feature_importances))
+pos = np.arange(index_sorted.shape[0]) + 0.5
+# Побудова діаграми 
+plt.figure()
+plt.bar(pos, feature_importances[index_sorted], align='center')
+plt.xticks(pos, feature_names[index_sorted])
+plt.ylabel('Relative Importance')
+plt.title('Оцінка важливості ознак за допомогою AdaBoost')
+plt.show()
